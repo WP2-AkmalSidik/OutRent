56e32b95b8e2aab4618ee55c655405bf0c748957
Akmal Sidik <143138356+WP2-AkmalSidik@users.noreply.github.com>
2025-01-22 14:41:09 +0700

    feat(migration): menambahkan table baru kedalam database

---
+++ database/migrations/2025_01_22_142340_create_bookings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->enum('status', ['pending', 'confirmed', 'canceled', 'completed'])->default('pending');
+            $table->enum('delivery_option', ['pickup', 'delivery']);
+            $table->decimal('total_price', 10, 2);
+            $table->decimal('deposit_paid', 10, 2)->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
